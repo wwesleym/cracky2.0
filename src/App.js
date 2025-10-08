368a1103f7a28b4bc9b5a597954ac41ce1a76bae
wwesleym <wesleyma007@gmail.com>
2025-10-08 00:17:31 -0700

    Change back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import CrackyHome from './CrackyHome'
 import CrackyEndless from './CrackyEndless'
 
@@ -7,12 +7,12 @@ export default function App() {
 
   return (
     <main>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path="/" element={<CrackyHome />} />
           <Route exact path="/endless" element={<CrackyEndless />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </main>
   )
 }
